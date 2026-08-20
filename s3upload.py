455f486d134076043c967afd3d2423b91528f8d3
YG7821 <yogyata_yogita@yahoo.co.in>
2026-08-20 10:47:26 +0530

    Fix file path in S3 upload function

--- s3upload.py
+++ s3upload.py
@@ -1,3 +1,3 @@
 import boto3
 s3 = boto3.resource('s3')
-s3.meta.client.upload_file(r"C:\Users\Yogita Gupta\Documents\B44\20thAug\s3upload", "yogitagupta-devopsbucket", "hello.txt")
+s3.meta.client.upload_file(r"C:\Users\Yogita Gupta\Documents\B44\20thAug\s3upload\boto3", 'yogitagupta-devopsbucket', 'hello.txt')
